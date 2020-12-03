430ca594eeda0e9e0feec7e4ef15a8500400182a
Adam Ross Russell <adamrr724@yahoo.com>
2020-12-03 14:04:39 -0800

    Small tweaks & fixes

--- rh_recommendations.py
+++ rh_recommendations.py
@@ -16,35 +16,20 @@ for stock in top_100:
     symbol = stock['symbol']
     # Get the ratings for that particular stock using the Robin Stocks get_rating function
     ratings_array = r.stocks.get_ratings(symbol)
-    
-    # This is a try/catch put in place to weed out unwanted results since the data returned can be a little bit inconsistent
-    try:
-        # This checks to see if the num_buy_rating key exists in the ratings array returned from above
-        ratings_array['num_buy_ratings'] 
-        
-        # If it does, the next two lines sum the # of ratings, then find the percentage of buys
-        ratings_sum = sum([ratings_array['num_buy_ratings'],ratings_array['num_hold_ratings'], ratings_array['num_sell_ratings']])
-        buy_percentage = ratings_array['num_buy_ratings']/ratings_sum
-        
-        # This collects the symbol and buy percentage and adds it to the final_results array
-        final_result.append({"symbol": symbol,"rating": buy_percentage,"total": ratings_sum})
-    
-    # This basically says: if the try above doesn't work and you get a KeyError exception (because the num_buyrating doesn't exist) then execute this part
-    except KeyError:
 
-        # Check to see if this is a result with zero ratings, if so -- don't include
-        if ratings_array["summary"] == None :
-            buy_percentage = "unknown"
+    # Check to see if this is a result with zero ratings, if so -- don't include
+    if ratings_array["summary"] == None :
+        buy_percentage = "No Ratings"
+    
+    # If it does have ratings then it will bypass the above and get caught in this else
+    else: 
         
-        # If it does have ratings then it will bypass the above and get caught in this else
-        else: 
-            
-            # Again, find the sum of results & buy percentage
-            ratings_sum = sum([ratings_array['summary']['num_buy_ratings'],ratings_array['summary']['num_hold_ratings'], ratings_array['summary']['num_sell_ratings']])
-            buy_percentage = ratings_array['summary']['num_buy_ratings']/ratings_sum
-            
-            # Add the symbol & buy_percentage to the final_results array
-            final_result.append({"symbol": symbol, "rating": buy_percentage,"total": ratings_sum})
+        # Find the sum of results & buy percentage
+        ratings_sum = sum([ratings_array['summary']['num_buy_ratings'],ratings_array['summary']['num_hold_ratings'], ratings_array['summary']['num_sell_ratings']])
+        buy_percentage = ratings_array['summary']['num_buy_ratings']/ratings_sum
+        
+        # Add the symbol & buy_percentage to the final_results array
+        final_result.append({"symbol": symbol, "rating": buy_percentage,"total": ratings_sum})
 
 # Define a function that is used to identify what key in the array should be sorted
 def mySort(e):
